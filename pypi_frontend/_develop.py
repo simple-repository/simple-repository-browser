9608e1dfd1175dc254ea9b6f4af2cd087e43a16c
Phil Elson <philip.elson@cern.ch>
2022-01-14 20:36:48 +0100

    Each clickable section in the project page updates the URL, and is therefore directly navigable.

--- pypi_frontend/_develop.py
+++ pypi_frontend/_develop.py
@@ -5,3 +5,11 @@ from . import _app
 
 logging.basicConfig(level=logging.DEBUG)
 app = _app.make_app(index_url='http://acc-py-repo.cern.ch/repository/vr-py-releases/simple')
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "pypi_frontend._develop:app", host="127.0.0.1", port=8000, reload=True,
+        log_level = "info",
+    )
