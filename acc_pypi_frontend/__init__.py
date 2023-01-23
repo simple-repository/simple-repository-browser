232e7ac56b1cbe4e6855323d03723445c1c9731d
Phil Elson <philip.elson@cern.ch>
2023-01-23 15:13:09 +0100

    Remove the only use of the work Acc-Py from the pypi_frontend implementation

--- acc_pypi_frontend/__init__.py
+++ acc_pypi_frontend/__init__.py
@@ -12,8 +12,4 @@ class AccPyCustomiser(base.Customiser):
     @classmethod
     def template_loader(cls) -> jinja2.BaseLoader:
         templates_dir = here / 'templates'
-        # if not (templates_dir / 'base').exists():
-        #     (templates_dir / 'base').symlink_to(base.here / 'templates')
-
-        loader = jinja2.FileSystemLoader([templates_dir, base.here / 'templates'])
-        return loader
+        return jinja2.FileSystemLoader([templates_dir, base.here / 'templates'])
