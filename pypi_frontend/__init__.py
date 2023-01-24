3e560307fe372d667c655db518c5cfad21a0acf4
Phil Elson <philip.elson@cern.ch>
2023-01-24 14:12:51 +0100

    Correct a spurious pre-commit issue

--- pypi_frontend/__init__.py
+++ pypi_frontend/__init__.py
@@ -3,6 +3,6 @@ Documentation for the pypi_frontend package
 
 """
 
-from . import _version
+from . import _version  # type: ignore
 
-__version__ = _version.version
+__version__ = _version.version  # type: ignore
