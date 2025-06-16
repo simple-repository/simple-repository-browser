14dcce6cdb76bbeba11640fa977a6aaa5766a653
Phil Elson <philip.elson@cern.ch>
2025-06-16 16:26:36 +0200

    Be more specific with the vendoring, and use defered everywhere where loading JS

--- javascript/webpack.config.js
+++ javascript/webpack.config.js
@@ -1,25 +1,33 @@
 const path = require('path');
 
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
+
 module.exports = {
   entry: {
-    'simple-repository-browser.core.js': './simple-repository-browser.core.js',
-    'simple-repository-browser.project.js': './simple-repository-browser.project.js',
+    'bootstrap.customized.js': './bootstrap.customized.js',
   },
   output: {
     filename: '[name]',
-    path: path.resolve(__dirname, '../simple_repository_browser/static/js/')
+    path: path.resolve(__dirname, '../simple_repository_browser/static/vendored/')
   },
   resolve: {
     extensions: [".ts", ".js"],
   },
+  plugins: [
+    new miniCssExtractPlugin()
+  ],
   module: {
     rules: [
       {
         test: /\.(scss)$/,
         use: [
+          // {
+          //   // Adds CSS to the DOM by injecting a `<style>` tag
+          //   loader: 'style-loader'
+          // },
           {
-            // Adds CSS to the DOM by injecting a `<style>` tag
-            loader: 'style-loader'
+            // Extracts CSS for each JS file that includes CSS
+            loader: miniCssExtractPlugin.loader
           },
           {
             // Interprets `@import` and `url()` like `import/require()` and will resolve them
