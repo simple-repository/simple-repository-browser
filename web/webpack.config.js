d16553a1cbe9c8b61480b28832d0198ee1a53d9a
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2025-11-20 10:05:07 +0100

    Suppress sass warnings in bootstrap code

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -51,7 +51,17 @@ module.exports = {
           },
           {
             // Loads a SASS/SCSS file and compiles it to CSS
-            loader: 'sass-loader'
+            loader: 'sass-loader',
+            options: {
+              // Options passed directly to Dart Sass
+              sassOptions: {
+                silenceDeprecations: [
+                  "global-builtin",
+                  "color-functions",
+                  "import"
+                ]
+              }
+            }
           }
         ]
       }
